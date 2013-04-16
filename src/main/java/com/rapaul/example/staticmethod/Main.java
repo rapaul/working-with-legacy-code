463f539fa7afcddec7c940e00d8d14ea3681d1ef
Richard Paul <richard.a.paul@gmail.com>
2013-04-16 23:08:38 +1200

    Made the Main method simpler to test through a safe IDE refactoring

--- src/main/java/com/rapaul/example/staticmethod/Main.java
+++ src/main/java/com/rapaul/example/staticmethod/Main.java
@@ -1,10 +1,16 @@
 package com.rapaul.example.staticmethod;
 
+import java.io.PrintStream;
+
 public class Main {
-	
+
 	public static void main(String... args) {
+		main(System.out);
+	}
+
+	protected static void main(PrintStream out) {
 		AggregationDisplay aggregationDisplay = new AggregationDisplay(new ResultAggregator());
-		System.out.println(aggregationDisplay.displayAggregationFor(new Patient("123")));
+		out.println(aggregationDisplay.displayAggregationFor(new Patient("123")));
 	}
 
 }
