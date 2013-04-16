5cc02844d38e209570b38511d242432fe17e8287
Richard Paul <richard.a.paul@gmail.com>
2013-04-16 22:53:39 +1200

    ResultManager now hits a database, but it makes things really slow
    We have 2 options at this stage, we can edit and pray.
    Or we can be responsible and use the cover and modify approach.
    Start by writing a higher level test that proves the application will
    hold together after we make a change.
    
    Try introducing a collaborator to the ResultAggregator for fetching
    results. A simple implementation of this results fetcher can simply
    delegate to the existing ResultManager, but at least we've now isolated
    our ResultAggregator code from the rest of the stack.
    
    Having this higher level test lets us make the changes safely, once we
    are confident our change was successful we can optionally (depending on
    the situation) delete the higher level test, relying on the individual
    unit tests from now on.

--- src/main/java/com/rapaul/example/staticmethod/ResultManager.java
+++ src/main/java/com/rapaul/example/staticmethod/ResultManager.java
@@ -1,11 +1,25 @@
 package com.rapaul.example.staticmethod;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ResultManager {
-	
+
 	public static List<Result> getResultsFor(Patient patient) {
-		throw new IllegalStateException("Can not connect to the result database");
+		simulateNastySlowIntegrationTest();
+		return Arrays.asList(new Result(1), new Result(2));
 	}
-	
+
+	private static void simulateNastySlowIntegrationTest() {
+		sleepFor10Seconds();
+		System.out.println("Time to go make a cup of tea!");
+		sleepFor10Seconds();
+	}
+
+	private static void sleepFor10Seconds() {
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) { }
+	}
+
 }
